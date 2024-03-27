ab08b902dde36a161bf348abd82c1ef7995933e2
Matheus de Lara Calache <mlcalache@gmail.com>
2024-03-27 15:23:32 +0100

    Add initial examples of pie chart and bar chart using MatPlotLib.

---
+++ 1 Initial Examples of MatPlotLib/pie_chart_example.py
@@ -0,0 +1,16 @@
+# Import libraries
+from matplotlib import pyplot as plt
+import numpy as np
+
+# Creating dataset
+cars = ['Audi', 'Bmw', 'Ford',
+        'Dodge', 'Porche', 'Mercedes']
+
+data = [23, 17, 35, 29, 12, 41]
+
+# Creating plot
+fig = plt.figure(figsize=(10, 7))
+plt.pie(data, labels=cars)
+
+# show plot
+plt.show()
